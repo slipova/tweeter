0da742052f695b6e829fe885c59eef69f78f3486
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-17 19:55:30 +0000

    implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,35 +19,71 @@ const tweetData = {
 
 
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+
+
+
 $(document).ready(function() {
-  // create new DOM element: article
+
+
   const createTweetElement = function(tweetData) {
 
-
-    // create structure for different parts of the article: header, tweet-text, footer
     return (`<article class="tweet">
-  <header><div class="image-name">
-  <img src="${tweetData.user.avatars}"><div>${tweetData.user.name}</div></div><div>${tweetData.user.handle}</div>
-  </header>
+    <header><div class="image-name">
+    <img src="${tweetData.user.avatars}"><div>${tweetData.user.name}</div></div><div>${tweetData.user.handle}</div>
+    </header>
     <p>${tweetData.content.text}</p>
     <footer>
-          <div class="time">${tweetData.created_at}</div>
-
-          <div class="icons">
-            <i class="fas fa-flag icons"></i>
-            <i class="fas fa-retweet icons"></i>
-            <i class="fas fa-heart icons"></i>
-          </div>
-        </footer>
-        </article>`);
+    <div class="time">${timeago.format(tweetData.created_at)}</div>
+    
+    <div class="icons">
+    <i class="fas fa-flag icons"></i>
+    <i class="fas fa-retweet icons"></i>
+    <i class="fas fa-heart icons"></i>
+    </div>
+    </footer>
+    </article>`);
   }
-
 
   const $tweet = createTweetElement(tweetData);
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
+  // console.log($tweet);
   $('#tweets-container').append($tweet);
+
+
+
+  const renderTweets = function(tweets) {
+    tweets.forEach((tweetObj) => $('#tweets-container').append(createTweetElement(tweetObj)))
+  }
+
+  const $tweet2 = renderTweets(data);
+  // console.log($tweet2);
 })
 
 
