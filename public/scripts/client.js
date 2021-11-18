6e42aa3c7a28ef35114806a2631e8a90fb1c2d14
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-18 04:22:51 +0000

    replace alert with sliding error messages

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,14 +21,19 @@ const tweetData = {
 
 $(document).ready(function() {
 
-
   const createTweetElement = function(tweetData) {
+    //cross-site scripting
+    const escape = function(str) {
+      let p = document.createElement("p");
+      p.appendChild(document.createTextNode(str));
+      return p.innerHTML;
+    };
 
     return (`<article class="tweet">
     <header><div class="image-name">
     <img src="${tweetData.user.avatars}"><div>${tweetData.user.name}</div></div><div>${tweetData.user.handle}</div>
     </header>
-    <p>${tweetData.content.text}</p>
+    <p>${escape(tweetData.content.text)}</p>
     <footer>
     <div class="time">${timeago.format(tweetData.created_at)}</div>
     
@@ -42,30 +47,30 @@ $(document).ready(function() {
   }
 
   const $tweet = createTweetElement(tweetData);
-
-  // console.log($tweet);
   $('#tweets-container').append($tweet);
 
 
-
   const renderTweets = function(tweets) {
-
     tweets.forEach((tweetObj) => $('#tweets-container').prepend(createTweetElement(tweetObj)))
   }
-
-
 
 
   $("form").submit((event) => {
     event.preventDefault();
 
+    const $errorMessage = $("#error")
     let data = $(".create-tweet").serialize();
-    if (data === "text=" || data === null) {
-      alert("Please enter a message")
-    } else if (data.length > 145) {
-      alert("Maximum characters exceeded")
-    } else {
 
+    if (data === "text=" || data === null) {
+      $errorMessage.show();
+      $errorMessage.html("Please enter a message")
+
+    } else if (data.length > 145) {
+      $errorMessage.show();
+      return $errorMessage.html("Maximum characters exceeded")
+
+    } else {
+      $errorMessage.hide()
       $.post("/tweets", data)
         .then(() => {
           loadTweets();
