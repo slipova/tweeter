f16f2074db05c1400baab7ba4d50af984cd6a87e
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2022-07-25 13:58:33 +0000

    initial set up of the game

---
+++ katas/kata21/kata21-number-guesser.js
@@ -0,0 +1,23 @@
+function getRandomInt() {
+  return Math.floor(Math.random() * 100);
+}
+
+let numberToGuess = getRandomInt();
+
+let prompt = require("prompt-sync")();
+
+
+let answer = parseInt(prompt("Guess a number between 0 and 99: "));
+console.log("You answered: " + answer);
+
+while (answer !== numberToGuess) {
+  if (answer === NaN) {
+    answer = parseInt(prompt("Not a number, try again: "))
+  } else if (answer > numberToGuess) {
+    answer = parseInt(prompt("Too high! Guess again: "))
+  } else {
+    answer = parseInt(prompt("Too low! Guess again: "))
+  }
+}
+
+prompt("You win!")
